c56f5876e491e3d405e7cdf599b3be852881e589
Alano <alanosantosdev@gmail.com>
2022-07-01 15:37:16 -0300

    requisito 1: rota / criada

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        <Route path="/" component={ Home } />
+      </BrowserRouter>
+    );
   }
 }
 
