168ae0f4b9eb84e7b8b2cfbcfb79c7b25500bffd
Alano <alanosantosdev@gmail.com>
2022-07-01 15:39:56 -0300

    requisito 1: componente /search

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class App extends React.Component {
       <BrowserRouter>
         <p>TrybeTunes</p>
         <Route path="/" component={ Home } />
+        <Route path="/Search" component={ Search } />
       </BrowserRouter>
     );
   }
