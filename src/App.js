d120ef18a67ea411d05e7272182bb15cec28b118
Alano <alanosantosdev@gmail.com>
2022-07-01 15:49:33 -0300

    requisito 1: componente album criado

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Search from './pages/Search';
+import Album from './pages/Album';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <p>TrybeTunes</p>
         <Route path="/" component={ Home } />
         <Route path="/Search" component={ Search } />
+        <Route path="/Album/:id" render={ (props) => <Album { ...props } /> } />
       </BrowserRouter>
     );
   }
---
+++ src/pages/Album.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Album extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-album">
+        <h1>
+          oi
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Album;
