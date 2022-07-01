005d876567893d7d1cd3ad74f6c724d427454c2b
Alano <alanosantosdev@gmail.com>
2022-07-01 16:02:19 -0300

    resquisito 1: Componente /edit criado

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 import Search from './pages/Search';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import Edit from './pages/Edit';
 
 class App extends React.Component {
   render() {
@@ -14,6 +16,8 @@ class App extends React.Component {
         <Route path="/Search" component={ Search } />
         <Route path="/Album/:id" render={ (props) => <Album { ...props } /> } />
         <Route path="/Favorites" component={ Favorites } />
+        <Route path="/Profile" component={ Profile } />
+        <Route path="/Profile/Edit" component={ Edit } />
       </BrowserRouter>
     );
   }
---
+++ src/pages/Edit.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Edit extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        <h1>oi</h1>
+      </div>
+    );
+  }
+}
+
+export default Edit;
