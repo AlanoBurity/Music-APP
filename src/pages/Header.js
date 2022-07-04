1b5b5a86869192c70c3a24a1a1e11d990f1549c2
AlanoBurity <alanosantosdev@gmail.com>
2022-07-04 16:08:03 -0300

    iniciando o requisito 3 e ajustes no requisito 2

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
     return (
-      <div>
-        <p>header</p>
-      </div>
+      <header datatestid="header-component">
+        <div>
+          <h2>TRYBETUNES</h2>
+        </div>
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Músicas</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
+          <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
+        </nav>
+      </header>
     );
   }
 }
